bdb974ceef6f95e5930921466adfbe5950af5e04
Татьяна Андреева <tan-andreeva@mail.ru>
2025-10-16 16:23:52 +0400

    add .

--- src/api/artistsApi.ts
+++ src/api/artistsApi.ts
@@ -1,7 +1,6 @@
 import baseQuery from '@/api/basequery';
 import { createApi } from '@reduxjs/toolkit/query/react';
 
-// import { authInterceptor } from '@/interceptors/interceptors';
 import IArtist, { IPainting } from '@/types/Artist';
 
 export const artistsApi = createApi({
